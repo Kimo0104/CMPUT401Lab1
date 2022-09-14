59686192ca4c0fbb921eeb57a0364b9246c7687a
Kareem Elakkad <elakkad@cs.ualberta.ca>
2022-09-14 17:36:22 -0600

    last commit

--- requests_version.py
+++ requests_version.py
@@ -1,3 +1,4 @@
 import requests
-import sys
+
 print(requests.get("http://www.google.com"))
+print(requests.get("https://raw.githubusercontent.com/Kimo0104/CMPUT401Lab1/master/requests_version.py").text)
